24786fe95f1054eccad1befb1c1280cd107db1ed
seanhawkridge <sean@hawkridge.co>
2016-02-23 16:46:40 +0000

    adding data vis

--- app/public/d3datavis.js
+++ app/public/d3datavis.js
@@ -1,17 +1,42 @@
 d3.json("http://localhost:9292/stats", function(data){
 
-var width = 500;
-var height = 500;
+var width = 1000;
+var height = 1000;
+
+var widthScale = d3.scale.linear()
+                  .domain([1949, 2015])
+                  .range([0, width]);
+
+  var axis = d3.svg.axis()
+            .scale(widthScale)
+            .ticks(10)
+
+  var heightScale = d3.scale.linear()
+                    .domain([1949, 2015])
+                    .range([0, height]);
+
+  var colorScale = d3.scale.linear()
+                    .domain([1949, 2015])
+                    .range(["red", "black"]);
 
   var canvas = d3.select("#visualisation").append("svg")
               .attr("width", width)
-              .attr("height", height);
+              .attr("height", height)
+              .append("g");
 
-  var circles = canvas.selectAll("circle")
+  canvas.append("g")
+        .attr("transform", "translate(0,height)")
+        .call(axis)
+
+  var circle = canvas.selectAll("rect")
                 .data(data)
                 .enter()
-                  .append("circle")
-                  .attr("r", function(d) {return d.year;});
+                  .append("rect")
+                  .attr("width", function(d){return widthScale(d.year);})
+                  .attr("height", 5)
+                  .attr("y", function(d,i) {return i*10;})
+                  .attr("fill", function(d){return colorScale(d.year);})
+
 
 
 
