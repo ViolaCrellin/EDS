4cbff4471cff3662b10a17fad9d3ee0c67907eac
ViolaCrellin <viola.crellin@gmail.com>
2016-02-23 11:31:56 +0000

    Merge again

--- EDS/app/data_mapper_setup.rb
+++
@@ -1,6 +0,0 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
-require_relative 'models/stats'
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/eds_api_#{ENV['RACK_ENV']}")
-DataMapper.finalize
