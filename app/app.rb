352ef31a92741b7a33ca02921be94ef572dd84f0
ViolaCrellin <viola.crellin@gmail.com>
2016-02-26 11:02:30 +0000

    Ready for 'selection' layer on top of stats

--- app/app.rb
+++ app/app.rb
@@ -19,32 +19,7 @@ class EDS < Sinatra::Base
     Stats.all(:year => (start.to_i..finish.to_i)).to_json
   end
 
-  # post '/stats' do
-  #   start = params[:start]
-  #   finish = params[:finish]
-  #
-  #   @stats = [start.to_i, finish.to_i]
-  #   # Stats.get(:year => (start.to_i..finish.to_i))
-  #   session[:selection] = @stats
-  #   redirect '/'
-  # end
 
-  # get 'stats/search' do
-  #   'hello world'
-  # end
-
-  # get 'stats/result' do
-  #   content_type :json
-  #   session[:selection].to_json
-  #   # send_file 'eds.html'
-  # end
-
-  # get '/stats/years' do
-  #   # Stats.get(:year => '1999').to_json
-  #   session[:selection].to_json
-  # end
-
-  # start the server if ruby file executed directly
   run! if app_file == $0
 
 end
