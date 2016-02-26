658854bc4c39aa87e56deda490d33d4c7ad433b3
ViolaCrellin <viola.crellin@gmail.com>
2016-02-26 15:02:50 +0000

    Some routes added

--- app/app.rb
+++ app/app.rb
@@ -13,9 +13,7 @@ class EDS < Sinatra::Base
   end
 
   get '/stats' do
-    start = params[:start]
-    finish = params[:finish]
-    Stats.all(:year => (start.to_i..finish.to_i)).to_json
+    filter_year(params[:start], params[:finish]).to_json
   end
 
   get '/gov' do
@@ -31,12 +29,38 @@ class EDS < Sinatra::Base
   end
 
   get '/debt' do
-    # start = params[:start]
-    # finish = params[:finish]
-    Stats.get("net_national_debt_billions").to_json
-    # year_selection.net_national_debt_billions.to_json
+    filter_year(params[:start], params[:finish]).debt.to_json
   end
 
+  get '/household' do
+    filter_year(params[:start], params[:finish]).household.to_json
+  end
+
+  get '/unemployment' do
+      filter_year(params[:start], params[:finish]).unemployment.to_json
+  end
+
+  get '/unemployment/rate' do
+      year_selection = filter_year(params[:start], params[:finish]).unemployment
+      year_selection.all(:unemployment_rate.gt => params[:rate].to_i).to_json
+  end
+
+  get '/unemployment/raw_thousands' do
+      year_selection = filter_year(params[:start], params[:finish]).unemployment
+      year_selection.all(:unemployment_thousands.gt => params[:raw_thousands].to_i).to_json
+  end
+
+  get '/macrostat/inflation' do
+    filter_year(params[:start], params[:finish]).macrostat.to_json
+  end
+
+  helpers do
+    def filter_year(start, finish)
+      start = "1948" if start.nil?
+      finish = "2015" if finish.nil?
+      Stats.all(:year => (start.to_i..finish.to_i))
+    end
+  end
 
   run! if app_file == $0
 
--- app/models/stats.rb
+++ app/models/stats.rb
@@ -11,7 +11,6 @@ include DataMapper::Resource
   has 1, :debt
   has 1, :household
   has 1, :unemployment
-
   has 1, :macrostat
 
 end
